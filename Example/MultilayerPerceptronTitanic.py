b82cbeb86a117560a3182d15cb9e7c6f6adb186e
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-04-09 17:22:08 +0200

    Update MultilayerPerceptronTitanic.py

--- Example/MultilayerPerceptronTitanic.py
+++ Example/MultilayerPerceptronTitanic.py
@@ -4,13 +4,17 @@ from __future__ import print_function
 
 import tensorflow as tf
 import sonnet as snt 
+import pandas as pd 
 
+train_data = get_training_data()
+test_data = get_test_data()
 
-train_data =
-test_data =
+def get_training_data():
+  data = pd.read_csv('/titanic_train.csv')
+  return data 
 
 linear_to_hidden = snt.Linear(output_size = 50 , name ='inp_to_hidden')
-hidden_to_out = snt.Linear(output_size = 25, name = 'hidden_to_out')
+hidden_to_out = snt.Linear(output_size = 2, name = 'hidden_to_out')
 
 #Sequential is a module wich applies a number of inner ops in sequence to data 
 #We can put another activation function like tf.tahn or tf.nn.relu 

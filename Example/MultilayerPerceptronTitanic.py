c0bcbb9d6d4e2e05265cd1fbe2330f72a61cb755
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-04-09 16:22:33 +0200

    Create MultilayerPerceptronTitanic.py

---
+++ Example/MultilayerPerceptronTitanic.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import sonnet as snt 
+
+
+train_data =
+test_data =
+
+linear_to_hidden = snt.Linear(output_size = 50 , name ='inp_to_hidden')
+hidden_to_out = snt.Linear(output_size = 25, name = 'hidden_to_out')
+
+#Sequential is a module wich applies a number of inner ops in sequence to data 
+#We can put another activation function like tf.tahn or tf.nn.relu 
+mlp = snt.Sequential([linear_to_hidden, tf.sigmoid, hidden_to_out])
+
+train_predictions = mlp(train_data)
+test_predictions = mlp(test_data)
